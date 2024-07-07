840beb1019d8857fe5ef12ec7c488416cbb6d6b7
Sneha1918 <deshpandesneha1918@gmail.com>
2024-07-07 22:08:49 +0530

    7July2024 ReadProperties and launchBrowser and constants files

---
+++ src/test/java/com/Utility/Constants.java
@@ -0,0 +1,9 @@
+package com.Utility;
+
+public class Constants 
+{
+	
+	public static final String Glacier_Sun_Glasses_Qty="3";
+	public static final int ImplicitWaitTimeOut=30;
+	public static final int UnitPriceSubStringIndex=2;
+}
